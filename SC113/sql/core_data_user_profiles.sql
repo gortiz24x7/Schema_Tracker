bfa2627d0eb608c0f9a2812abd77f1fb055471b3
Gus Ortiz <gortiz@elementcorp.com>
2017-02-23 12:09:51 -0600

    Added Inset Test Row

--- SC113/sql/core_data_user_profiles.sql
+++ SC113/sql/core_data_user_profiles.sql
@@ -1,3 +1,4 @@
+-- Added TEST Row
 USE trnd1_spin_work
 GO
 INSERT INTO trnd1_spin_work..core_data_user_profiles
@@ -14,6 +15,8 @@ INSERT INTO trnd1_spin_work..core_data_user_profiles
 VALUES ('TELEMATICS_MOBI_INT_OPS_CONTEN')
 INSERT INTO trnd1_spin_work..core_data_user_profiles
 VALUES ('TELEMATICS_MOBI_INT_OPS_DEFAUL')
+INSERT INTO trnd1_spin_work..core_data_user_profiles
+VALUES ('TEST')
 GO
 grant select  on dbo.core_data_user_profiles to spin_read_role
 GO
